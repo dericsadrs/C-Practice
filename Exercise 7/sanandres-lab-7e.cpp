5110a014fe8b92b30033ae7b3c03265b1f3f7ef4
Deric San Andres <105538170+dericsadrs@users.noreply.github.com>
2022-12-17 09:55:07 +0800

    Changes

--- Exercise 7/sanandres-lab-7e.cpp
+++ Exercise 7/sanandres-lab-7e.cpp
@@ -1,25 +1,4 @@
-//******************************************************************
-// Filename	: picaso-lab-7c.cpp
-// Date		: 15 October 2019
-// Subject	: CSDC101 - Fundamentals of Programming
-// First Semester, SY 2019 - 2020
-// Activity	: Laboratory Exercise 7
-// Problem Title: Health Bar
-// Honor Code	:
-// 	This is my own program. I have not received any
-//      unauthorized help in completing this work. I have not
-//      copied from my classmate, friend, nor any unauthorized
-//      resource. I am well aware of the policies stipulated
-//      in the handbook regarding academic dishonesty.
-//      If proven guilty, I won't be credited any points for
-//      this exercise.
-//
-// Complete Name: Joeliam C. Picaso
-// ID Number	: 2019-1-0780
-// Year-Course	: 1-BSIT
-// DCS, College of Computer Studies
-// Ateneo de Naga University
-//******************************************************************
+
 #include <iostream>
 #include <cmath>
 
